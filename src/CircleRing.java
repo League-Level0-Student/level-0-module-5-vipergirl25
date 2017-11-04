38ed02c6650d46523337e764fda290eb81731f9e
League <League@administorsimac.attlocal.net>
2017-11-04 16:05:55 -0700

    circlering, code4life

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,27 +9,45 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
+    	Robot rob = new Robot();
 
 
        //  2. Set your robot’s position to x=150 and y=200
+    	rob.setX(150);
+    	rob.setY(200);
 
         // 3. Put the robot's pen down
+    	rob.penDown();
 
         // 4. Set the robot’s speed to 10
+    	rob.setSpeed(100);
 
         // 5. Do everything below here 360 times (use i as the counter)
+    	for (int i = 0; i < 369; i++) {
+			
+		
 
 	        // 6. Move the robot 3 pixels
+    	rob.move(3);
 
        //  7. Turn the robot 1 degree
+    	rob.turn(1);
 
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)
+    	if(i%20==0) {
+    		for (int j = 0; j < 360; j++) {
+				
+			}
 
 
         // 9. Do steps 10 thru 12, 360 times (use j as the counter)
 
 		        // 10. Move the robot 1 pixel
+    		rob.move(1);
 
 		        // 11. Turn the robot 1 degree
+    		rob.turn(1);
+    	}
+    	}
 }
 }
