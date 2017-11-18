d881a807d382301981e3d3ac772e0e926c6c9347
League <League@administorsimac.attlocal.net>
2017-11-18 15:57:57 -0800

    prime or not, forloopgauntlet, practice test

--- src/primeornot.java
+++ src/primeornot.java
@@ -1,14 +1,25 @@
 import javax.swing.JOptionPane;
 
 public class primeornot {
-	public static void main(String[] args) {
-		int usernumber = JOptionPane.showMessageDialog(null, "Enter a number:");
-		for (int i = 0; i < usernumber; i++) {
-				int divisiblenumber = usernumber-i;
-				if(usernumber%divisiblenumber==0) {
-					System.out.println("not prime");	
-					}
-		
+public static void main(String[] args) {
+	String primeornotnumber = JOptionPane.showInputDialog("Enter a number to see if it is prime or not?");
+	int number = Integer.parseInt(primeornotnumber);
+	boolean prime = true;
+	for (int i = 2; i < Math.sqrt(number); i++) {
+		int thingy = number%i;
+		if(thingy==0) {
+			prime = false;
+			break;
+		}
+			
+		}
+	if (prime) {
+		System.out.println("Number is prime.");
 	}
-
+	else {
+		System.out.println("number is not prime.");
+	}
+	}
 }
+
+
